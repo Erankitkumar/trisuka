dc885551331a77e608ce6475de5245a9e76151a7
abhi0661236 <ap0661236@gmail.com>
2021-09-15 16:45:44 +0530

    improving the project structure

---
+++ components/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- include libraries here -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
+    <title>Trisuka</title>
+
+    <!-- custom css goes here -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    
+
+
+
+
+    <!-- scripts libraries are here -->
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <!-- custom scripts are here -->
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include './components/navbar.php';
+?>
+
+
+<div class="container">
+    <h1 class="text-center bg-dark text-white">
+        Hello there...
+    </h1>
+</div>
